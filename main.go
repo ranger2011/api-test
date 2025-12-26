5a77625974c1da37e90defd50c28f87ec864d18a
ranger <wu_weiguo@hotmail.com>
2025-12-25 22:48:13 -0800

    delete recipe

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func main() {
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
+	router.DELETE("/recipes/:id", DeleteRecipeHandler)
 	err := router.Run(":8080")
 	if err != nil {
 		fmt.Println(err)
@@ -77,4 +78,21 @@ func UpdateRecipeHandler(c *gin.Context) {
 	}
 	recipes[index] = recipe
 	c.JSON(http.StatusOK, recipe)
+}
+
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+			break
+		}
+	}
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "recipe not found"})
+		return
+	}
+	recipes = append(recipes[:index], recipes[index+1:]...)
+	c.JSON(http.StatusOK, recipes)
 }
